9d62a866360d3e050feb23d1df33a2727f93875b
ultramailman <cyrus9212@gmail.com>
2017-01-19 00:02:00 -0800

    Added styles for song items and helper functions to create title-subtitles

--- music-app.js
+++ music-app.js
@@ -17,6 +17,8 @@ function redraw() {
   redrawTopBar();
   if(currentTab === 'playlists') {
     redrawPlaylistsContent();
+  } else if(currentTab === 'library') {
+    redrawLibraryContent();
   }
 }
 
@@ -38,14 +40,58 @@ function removeAllChildren(ele) {
     ele.removeChild(ele.lastChild);
   }
 }
+function redrawLibraryContent() {
+  var musicList = document.createElement('ul');
+  musicList.classList.add('music-item-list');
+  window.MUSIC_DATA['songs'].forEach(function(song) {
+    musicList.appendChild(createSongItemNode(song));
+  });
+}
+function createSongItemNode(song) {
+  var item = document.createElement('li');
+  item.classList.add('music-item');
+
+  var musicIcon = document.createElement('div');
+  musicIcon.classList.add('music-cover-thumbnail');
+
+  var expandButton = document.createElement('span');
+  expandButton.classList.add('music-item-button');
+  addGlyphicon(expandButton, 'chevron-right');
+
+  item.appendChild(musicIcon);
+  item.appendChild(createMusicTitleSubtitleNode(song['title'], song['artist']));
+  item.appendChild(expandButton);
+
+  return item;
+}
+
+function createMusicTitleSubtitleNode(title, subtitle) {
+  var node = document.createElement('div');
+  node.classList.add('music-title-subtitle');
+  node.appendChild(createMusicTitleNode(title));
+  node.appendChild(createMusicSubtitleNode(subtitle));
+  return node;
+}
+
+function createMusicTitleNode(text) {
+  var musicTitle = document.createElement('span');
+  musicTitle.classList.add('music-title');
+  musicTitle.textContent = text;
+  return musicTitle;
+}
+
+function createMusicSubtitleNode(text) {
+  var musicSubtitle = document.createElement('span');
+  musicSubtitle.classList.add('music-subtitle');
+  musicSubtitle.textContent = text;
+  return musicSubtitle;
+}
 function redrawPlaylistsContent() {
   var button = createNewPlaylistButtonNode();
 
   var playlistList = document.createElement('ul');
-  playlistList.id = 'playlist-list';
   playlistList.classList.add('music-item-list');
-  var playlistListData = window.MUSIC_DATA['playlists'];
-  playlistListData.forEach(function(playlist) {
+  window.MUSIC_DATA['playlists'].forEach(function(playlist) {
     playlistList.appendChild(createPlaylistItemNode(playlist));
   });
 
@@ -77,17 +123,13 @@ function createPlaylistItemNode(playlist) {
   var musicIcon = document.createElement('div');
   musicIcon.classList.add('music-cover-thumbnail');
 
-  var musicTitle = document.createElement('span');
-  musicTitle.classList.add('music-title');
-  musicTitle.textContent = playlist['name'];
-
   var expandButton = document.createElement('span');
   expandButton.classList.add('music-item-button');
   addGlyphicon(expandButton, 'chevron-right');
 
 
   item.appendChild(musicIcon);
-  item.appendChild(musicTitle);
+  item.appendChild(createMusicTitleSubtitleNode(playlist['name']));
   item.appendChild(expandButton);
 
   return item;
