f1102c398fc79d609b59f387ab890a100ab19adf
ultramailman <cyrus9212@gmail.com>
2017-01-22 12:04:04 -0800

    Clicking a playlist now shows all the songs it contains

--- music-app.js
+++ music-app.js
@@ -26,9 +26,11 @@
   function redraw () {
     redrawTopBar();
     if (currentTab === 'playlists') {
-      redrawPlaylistsContent();
+      redrawPlaylistListContent();
     } else if (currentTab === 'library') {
       redrawLibraryContent();
+    } else if (currentTab === 'playlist-content') {
+      redrawPlaylistContent();
     }
   }
 
@@ -44,7 +46,56 @@
       }
     }
   }
+  function redrawLibraryContent () {
+    var musicList = document.createElement('ul');
+    musicList.classList.add('music-item-list');
 
+    musicList.appendChild(createSortMethodBar());
+    executeSort(window.MUSIC_DATA['songs']).forEach(function (song) {
+      musicList.appendChild(createSongItemNode(song));
+    });
+
+    var contentView = document.getElementById('content-view');
+    removeAllChildren(contentView);
+    contentView.appendChild(musicList);
+  }
+  function redrawPlaylistListContent () {
+    var button = createNewPlaylistButtonBarNode();
+
+    var playlistList = document.createElement('ul');
+    playlistList.classList.add('music-item-list');
+    playlistList.appendChild(button);
+    window.MUSIC_DATA['playlists'].forEach(function (playlist) {
+      playlistList.appendChild(createPlaylistItemNode(playlist));
+    });
+
+    var contentView = document.getElementById('content-view');
+    removeAllChildren(contentView);
+    contentView.appendChild(playlistList);
+  }
+  function redrawPlaylistContent () {
+    var playlist = window.MUSIC_DATA['playlists'].find(function (playlist) {
+      return playlist['id'] === currentSelectedPlaylistID;
+    });
+    var songMap = createID2SongMap();
+    var playlistTitle = createPlaylistTitleNode(playlist['name']);
+    var songList = document.createElement('ul');
+    songList.classList.add('music-item-list');
+    songList.appendChild(playlistTitle);
+    playlist['songs'].forEach(function (id) {
+      songList.appendChild(createSongItemNode(songMap[id]));
+    });
+    var contentView = document.getElementById('content-view');
+    removeAllChildren(contentView);
+    contentView.appendChild(songList);
+  }
+  function createID2SongMap () {
+    var songMap = [];
+    window.MUSIC_DATA['songs'].forEach(function (song) {
+      songMap[song['id']] = song;
+    });
+    return songMap;
+  }
   function prepareSortValue (str) {
     return str.trim().replace(/^The /, '').trim();
   }
@@ -70,10 +121,16 @@
       ele.removeChild(ele.lastChild);
     }
   }
+  function createPlaylistTitleNode (name) {
+    var title = document.createElement('span');
+    title.textContent = name;
+    title.classList.add('playlist-content-title');
+    return title;
+  }
   function createSortButton (sortKey) {
     var button = document.createElement('button');
     button.classList.add('purple-button');
-    button.classList.add('sort-method-button');
+    button.classList.add('content-view-button');
 
     var text = document.createElement('span');
     text.textContent = 'Sort by ' + sortKey;
@@ -87,39 +144,13 @@
   }
   function createSortMethodBar () {
     var bar = document.createElement('div');
-    bar.classList.add('sort-method-button-bar');
+    bar.classList.add('content-view-button-bar');
 
     bar.appendChild(createSortButton('artist'));
     bar.appendChild(createSortButton('title'));
     return bar;
   }
-  function redrawLibraryContent () {
-    var musicList = document.createElement('ul');
-    musicList.classList.add('music-item-list');
 
-    musicList.appendChild(createSortMethodBar());
-    executeSort(window.MUSIC_DATA['songs']).forEach(function (song) {
-      musicList.appendChild(createSongItemNode(song));
-    });
-
-    var contentView = document.getElementById('content-view');
-    removeAllChildren(contentView);
-    contentView.appendChild(musicList);
-  }
-  function redrawPlaylistsContent () {
-    var button = createNewPlaylistButtonNode();
-
-    var playlistList = document.createElement('ul');
-    playlistList.classList.add('music-item-list');
-    playlistList.appendChild(button);
-    window.MUSIC_DATA['playlists'].forEach(function (playlist) {
-      playlistList.appendChild(createPlaylistItemNode(playlist));
-    });
-
-    var contentView = document.getElementById('content-view');
-    removeAllChildren(contentView);
-    contentView.appendChild(playlistList);
-  }
   function executeAddToPlaylist () {
     if (currentSelectedPlaylistID !== null && currentSelectedSongID !== null) {
       window.MUSIC_DATA['playlists'].find(function match (playlist) {
@@ -236,8 +267,8 @@
 
   function createNewPlaylistButtonNode () {
     var button = document.createElement('button');
-    button.id = 'new-playlist-button';
     button.classList.add('purple-button');
+    button.classList.add('content-view-button');
 
     var icon = document.createElement('span');
     addGlyphicon(icon, 'plus');
@@ -247,8 +278,13 @@
 
     button.appendChild(icon);
     button.appendChild(text);
-
     return button;
+  }
+  function createNewPlaylistButtonBarNode () {
+    var bar = document.createElement('div');
+    bar.classList.add('content-view-button-bar');
+    bar.appendChild(createNewPlaylistButtonNode());
+    return bar;
   }
   function createPlaylistItemNode (playlist) {
     var item = document.createElement('li');
@@ -264,6 +300,11 @@
     item.appendChild(musicIcon);
     item.appendChild(createMusicTitleSubtitleNode(playlist['name']));
     item.appendChild(expandButton);
+    item.addEventListener('click', function (e) {
+      currentSelectedPlaylistID = playlist['id'];
+      currentTab = 'playlist-content';
+      redraw();
+    });
 
     return item;
   }
