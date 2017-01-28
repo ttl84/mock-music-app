6e2507fbb879b84194991afd200e9d7485f09316
ultramailman <cyrus9212@gmail.com>
2017-01-28 13:44:19 -0800

    Now routes everything correctly and works the same as the local version

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
 // Import the http library
-var http = require('http')
-var url = require('url')
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
 // Create a server and provide it a callback to be executed for every HTTP request
 // coming into localhost:3000.
 var server = http.createServer()
+var routes = []
+function route (pattern, callback) {
+  routes.push({
+    'pattern': pattern,
+    'callback': callback
+  })
+}
+function doRoute (request, response) {
+  var URL = url.parse(request.url, true)
+  routes.forEach(function (ele) {
+    if (URL.pathname.match(ele['pattern'])) {
+      ele['callback'](request, response)
+    }
+  })
+}
+route(/^\/$/, function (request, response) {
+  console.log('begin redirect')
+  response.statusCode = 301
+  response.setHeader('location', '/playlists')
+  response.end()
+  console.log('end redirect')
+})
+
+route(/^\/(playlists|library|search)$/, function (request, response) {
+  console.log('begin html')
+  response.statusCode = 200
+  response.setHeader('Content-Type', 'text/html')
+  var stream = fs.createReadStream('playlist.html')
+  stream.pipe(response)
+})
+
+route(/^\/playlist\.css$/, function (request, response) {
+  response.statusCode = 200
+  response.setHeader('content-type', 'text/css')
+  var stream = fs.createReadStream('playlist.css')
+  stream.pipe(response)
+})
+
+route(/^\/music-app\.js$/, function (request, response) {
+  response.statusCode = 200
+  response.setHeader('content-type', 'application/javascript')
+  var stream = fs.createReadStream('music-app.js')
+  stream.pipe(response)
+})
+
+route(/^\/music-data\.js$/, function (request, response) {
+  response.statusCode = 200
+  response.setHeader('content-type', 'application/javascript')
+  var stream = fs.createReadStream('music-data.js')
+  stream.pipe(response)
+})
 
 server.on('request', function (request, response) {
   response.setHeader('cache-control', 'public, max-age=1800')
-  var URL = url.parse(request.url, true)
-  if (URL.pathname.match(/^\/$/)) {
-    console.log('begin redirect')
-    response.statusCode = 301
-    response.setHeader('location', '/playlists')
 
-    response.end()
-    console.log('end redirect')
-  } else if (URL.pathname.match(/^\/playlists$/)) {
-    console.log('begin playlists')
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/plain')
-    response.end(request.url)
-    console.log('end playlists')
-  }
+  doRoute(request, response)
 })
 
 // Start the server on port 3000
