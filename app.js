7991636c0c7f65a8dc418c84713dd13d7c8f5e77
ultramailman <cyrus9212@gmail.com>
2017-01-28 12:04:28 -0800

    Can't use express yet

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var app = express();
+// Import the http library
+var http = require('http')
+var url = require('url')
+// Create a server and provide it a callback to be executed for every HTTP request
+// coming into localhost:3000.
+var server = http.createServer()
 
-app.get('/', function (request, response) {
-  response.redirect('/playlists');
-});
-app.get('/playlists', function (request, response) {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/html');
-  response.end('Amazing playlist');
-});
+server.on('request', function (request, response) {
+  var URL = url.parse(request.url, true)
+  if (URL.pathname.match('/playlists')) {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/plain')
+    response.end(request.url)
+  }
+})
 
 // Start the server on port 3000
-app.listen(3000, function () {
-  console.log('Amazing music app server listening on port 3000!');
-});
+server.listen(3000, function () {
+  console.log('Amazing music app server listening on port 3000!')
+})
