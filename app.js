37410fbeb3eb759a6970e1595c418246ab20d4c6
ultramailman <cyrus9212@gmail.com>
2017-01-28 11:31:32 -0800

    Redirect / to /playlists

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// Import the http library
-var http = require('http');
 var express = require('express');
 var app = express();
-// Create a server and provide it a callback to be executed for every HTTP request
-// coming into localhost:3000.
+
 app.get('/', function (request, response) {
+  response.redirect('/playlists');
+});
+app.get('/playlists', function (request, response) {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/html');
   response.end('Amazing playlist');
