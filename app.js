cb63f44911c95a00e18d60c74b179d194d1f7fef
ultramailman <cyrus9212@gmail.com>
2017-01-28 12:24:53 -0800

    now correctly caches and redirects

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ var url = require('url')
 var server = http.createServer()
 
 server.on('request', function (request, response) {
+  response.setHeader('cache-control', 'public, max-age=1800')
   var URL = url.parse(request.url, true)
-  if (URL.pathname.match('/playlists')) {
+  if (URL.pathname.match(/^\/$/)) {
+    console.log('begin redirect')
+    response.statusCode = 301
+    response.setHeader('location', '/playlists')
+
+    response.end()
+    console.log('end redirect')
+  } else if (URL.pathname.match(/^\/playlists$/)) {
+    console.log('begin playlists')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/plain')
     response.end(request.url)
+    console.log('end playlists')
   }
 })
 
